caf1da478f245e27c3616340598e658d79fb6c56
Andy Leap <andyleap@gmail.com>
2022-04-04 19:05:01 -0700

    working

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"io"
 	"log"
 	"net"
+	"net/netip"
 	"strings"
 	"sync"
+	"sync/atomic"
+	"time"
 
 	"github.com/jessevdk/go-flags"
 )
@@ -14,14 +17,17 @@ var options struct {
 	Routes []string `long:"route"`
 }
 
-type UDPTuple struct {
-	SrcIP net.IP
-	SrcPort uint16
-	DstIP net.IP
-	DstPort uint16
+type UDPProxyConn struct {
+	Src     netip.AddrPort
+	Dst     *net.UDPConn
+	timeout int64
+	close   chan struct{}
 }
 
-var udpMap = make(map[UDPTuple])
+type UDPProxy struct {
+	conns map[netip.AddrPort]*UDPProxyConn
+	lock  sync.RWMutex
+}
 
 func main() {
 	_, err := flags.Parse(&options)
@@ -40,14 +46,14 @@ func main() {
 			from = mode[1]
 			network = mode[0]
 		}
-		l, err := net.Listen(network, ":"+from)
-		if err != nil {
-			log.Fatal(err)
-		}
 		switch network {
 		case "tcp":
-			go func(to string, l net.Listener) {
+			go func(from, to string) {
 				defer mainCond.Broadcast()
+				l, err := net.Listen("tcp", ":"+from)
+				if err != nil {
+					log.Fatal(err)
+				}
 				for {
 					fc, err := l.Accept()
 					log.Println("forwarding to ", to)
@@ -89,18 +95,79 @@ func main() {
 						cond.Wait()
 					}(fc)
 				}
-			}(to, l)
+			}(from, to)
 		case "udp":
-			go func(to string, l net.Listener) {
+			udpProxy := &UDPProxy{
+				conns: map[netip.AddrPort]*UDPProxyConn{},
+			}
+			go func(from, to string, udpProxy *UDPProxy) {
 				defer mainCond.Broadcast()
+				fromUDP, err := net.ResolveUDPAddr("udp", ":"+from)
+				if err != nil {
+					log.Fatal(err)
+				}
+				toUDP, err := net.ResolveUDPAddr("udp", to)
+				if err != nil {
+					log.Fatal(err)
+				}
+				l, err := net.ListenUDP("udp", fromUDP)
+				if err != nil {
+					log.Fatal(err)
+				}
+				buf := make([]byte, 1500)
 				for {
-					fc, err := l.Accept()
-					log.Println("forwarding to ", to)
+					n, addr, err := l.ReadFromUDPAddrPort(buf)
 					if err != nil {
 						log.Fatal(err)
 					}
+					udpProxy.lock.RLock()
+					conn, ok := udpProxy.conns[addr]
+					udpProxy.lock.RUnlock()
+					if !ok {
+						udpProxy.lock.Lock()
+						conn, ok = udpProxy.conns[addr]
+						if !ok {
+							conn = &UDPProxyConn{
+								Src:     addr,
+								timeout: time.Now().Unix(),
+							}
+							conn.Dst, err = net.DialUDP("tcp", nil, toUDP)
+							if err != nil {
+								log.Fatal(err)
+							}
+							udpProxy.conns[addr] = conn
+							go func(c *UDPProxyConn) {
+								buf := make([]byte, 1500)
+								for {
+									n, err := conn.Dst.Read(buf)
+									if err != nil {
+										return
+									}
+									atomic.StoreInt64(&c.timeout, time.Now().Unix())
+									_, err = l.WriteToUDPAddrPort(buf[:n], c.Src)
+								}
+							}(conn)
+						}
+						udpProxy.lock.Unlock()
+					}
+					conn.Dst.Write(buf[:n])
+					atomic.StoreInt64(&conn.timeout, time.Now().Unix())
 				}
-			}(to, l)
+			}(from, to, udpProxy)
+			go func(udpProxy *UDPProxy) {
+				for {
+					udpProxy.lock.Lock()
+					t := time.Now().Unix() - 60
+					for _, conn := range udpProxy.conns {
+						if conn.timeout < t {
+							conn.Dst.Close()
+							delete(udpProxy.conns, conn.Src)
+						}
+					}
+					udpProxy.lock.Unlock()
+					time.Sleep(time.Second * 60)
+				}
+			}(udpProxy)
 		}
 	}
 	mainCond.Wait()
